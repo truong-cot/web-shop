a8f29e3330e8beecc8aa64740dba4dd717a8d711
truong-cot <engtruong1609@gmail.com>
2022-04-04 22:44:49 +0700

    upload search

--- components/pages/home/Search/index.js
+++ components/pages/home/Search/index.js
@@ -1,17 +1,37 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./Search.module.scss";
 import Button from "../../../../components/control/Button";
 import { BsSearch, BsChevronDown } from "react-icons/bs";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import SearchProduct from "./../../../../components/pages/search-product";
 
 function Search() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleClick = () => {
+    setKeyword("");
+  };
+
   return (
     <div className={styles.searchMain}>
       <div className="container">
         <div className={styles.container}>
           <div className={styles.item}>
+            {keyword !== "" && (
+              <div className="overlay" onClick={handleClick}></div>
+            )}
             <div className={styles.input}>
-              <input type="text" placeholder="Tìm kiếm" />
+              {keyword !== "" && <SearchProduct keyword={keyword} />}
+              <input
+                type="text"
+                placeholder="Tìm kiếm"
+                onChange={handleChange}
+                value={keyword}
+              />
               <div className={styles.icon}>
                 <BsSearch />
               </div>
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Banner from "../components/pages/home/Banner";
 import Search from "../components/pages/home/Search";
 import Classify from "../components/pages/home/Classify";
 import Products from "../components/pages/home/Products";
+import SearchProduct from "./../components/pages/search-product";
 
 export default function Home() {
   return (
