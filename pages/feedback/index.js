ccdc38648e726862c0513be493f8bda3ea622420
truong-cot <engtruong1609@gmail.com>
2022-05-02 18:33:20 +0700

    upload feedback

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -3,13 +3,25 @@ import styles from "./Feedback.module.scss";
 import NavProfile from "./../../components/pages/profile/NavProfile";
 import ContentProfile from "./../../components/pages/profile/ContentProfile";
 
+import Button from "./../../components/control/Button";
+
 function Feedback() {
   return (
     <div className={styles.container}>
       <div className="container">
         <div className={styles.main}>
           <NavProfile />
-          <ContentProfile>day la trang gop ý</ContentProfile>
+          <ContentProfile>
+            <form className={styles.form}>
+              <textarea
+                className={styles.text}
+                placeholder="Đóng góp ý kiến của bạn"
+              ></textarea>
+              <Button primary rounded product>
+                Gửi
+              </Button>
+            </form>
+          </ContentProfile>
         </div>
       </div>
     </div>
